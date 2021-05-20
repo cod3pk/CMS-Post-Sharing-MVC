0ee10562f40d2b94abceddab02ba339ce3e8c16a
Asfand Yar Ali Khan <asfandkhan404@gmail.com>
2021-05-20 17:02:24 +0500

    Testing is done and ready to be deployed !

---
+++ app/config/config.php
@@ -0,0 +1,15 @@
+<?php
+  // DB Params
+  define('DB_HOST', '__YOUR__HOST__');
+  define('DB_USER', '__YOUR__USERNAME__');
+  define('DB_PASS', '__YOUR__PASSWORD__');
+  define('DB_NAME', '__DATABSE__NAME__');
+
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', '__URL__OF__WEBSITE__');
+  // Site Name
+  define('SITENAME', '__SITENAME__');
+  // App Version
+  define('APPVERSION', '1.0.0');
